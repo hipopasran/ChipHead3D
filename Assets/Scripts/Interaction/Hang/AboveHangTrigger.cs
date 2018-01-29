e37dd7d81a4a1b860ace9cb3b1f1bccef6f27d0a
hipopasran <vladimirkeda@yandex.ru>
2018-01-29 22:48:18 +0300

    HangUp

---
+++ Assets/Scripts/Interaction/Hang/AboveHangTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboveHangTrigger : MonoBehaviour {
+    //component variable
+    public GameObject player;
+
+
+
+    //switching scripts
+    void Change()
+    {
+       // player.GetComponent<PlayerMovement>().enabled = !player.GetComponent<PlayerMovement>().enabled;
+        //player.GetComponent<Rigidbody>().useGravity = !player.GetComponent<Rigidbody>().useGravity;
+        //player.GetComponent<HangMovement>().enabled = !player.GetComponent<HangMovement>().enabled;
+    }
+
+
+
+    //when character enter the trigger collider
+    void OnTriggerEnter()
+    {
+        
+        Change();
+        player.GetComponent<HangMovement>().enabled = true;
+    }
+
+    //when character leave the trigger collider
+    void OnTriggerExit()
+    {
+        Change();
+        player.GetComponent<PlayerMovement>().enabled = true;
+        player.GetComponent<Rigidbody>().useGravity = true;
+        player.GetComponent<HangMovement>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/Player/HangMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangMovement : MonoBehaviour {
+
+    //character's variable
+    public GameObject player;
+    //component variables
+    Rigidbody playerRigidbody; //character's Rigidbody component
+    PlayerStats playerStats; //character's stats
+    void Change()
+    {
+        player.GetComponent<PlayerMovement>().enabled = !player.GetComponent<PlayerMovement>().enabled;
+        player.GetComponent<Rigidbody>().useGravity = !player.GetComponent<Rigidbody>().useGravity;
+       // player.GetComponent<HangMovement>().enabled = !player.GetComponent<HangMovement>().enabled;
+
+    }
+
+    void MoveUp()
+    {
+        
+        float verticalDirection = Input.GetAxis("Vertical");
+     
+
+        if(verticalDirection <0)
+        {
+            Input.ResetInputAxes();
+            player.GetComponent<PlayerMovement>().enabled = true;
+            player.GetComponent<Rigidbody>().useGravity = true;
+
+            Input.ResetInputAxes();
+
+        }
+        else if(verticalDirection>0)
+        {
+            Input.ResetInputAxes();
+            {
+                player.GetComponent<Transform>().position = new Vector3(transform.position.x + player.transform.lossyScale.x, transform.position.y + (2*player.transform.lossyScale.y), 0);
+                player.GetComponent<PlayerMovement>().enabled = true;
+                player.GetComponent<Rigidbody>().useGravity = true;
+            }
+            Input.ResetInputAxes();
+            
+            
+        }
+                     
+    }
+
+    void MoveDown()
+    {
+        float verticalDirection = Input.GetAxis("Vertical");
+        
+
+        if (verticalDirection < 0)
+        {
+            player.GetComponent<PlayerMovement>().enabled = false;
+            player.GetComponent<Rigidbody>().useGravity = false;
+            Input.ResetInputAxes();
+            
+            player.GetComponent<Transform>().position = new Vector3(transform.position.x - player.transform.lossyScale.x, transform.position.y - (2 * player.transform.lossyScale.y), 0);
+            
+            Input.ResetInputAxes();
+           
+        }
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name=="OnHang")
+        {
+            MoveUp();
+        }
+        else if(other.gameObject.name == "AboveHang")
+        {
+            MoveDown();
+        }
+
+    }
+
+    //use this for initialization
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerStats = GetComponent<PlayerStats>();
+    }
+
+    //update is called once per delta
+    void FixedUpdate()
+    {
+        
+    }
+}
